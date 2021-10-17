ae153faff2e77e68530573a2fe52e4af6c3ac16d
Danile_T <tianyuan@132>
2021-10-17 23:06:38 +0800

    存

--- daprA/Controllers/PublishSubscribe/PublishSubscribeController.cs
+++ daprA/Controllers/PublishSubscribe/PublishSubscribeController.cs
@@ -3,12 +3,34 @@ using Dapr.Client;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace daprA.Controllers.PublishSubscribe
 {
+    /// <summary>
+    /// dapr发布订阅 https://docs.dapr.io/zh-hans/developing-applications/building-blocks/pubsub/howto-publish-subscribe/#%E4%BB%8B%E7%BB%8D
+    /// ACK-ing 消息
+    /// 为了告诉Dapr 消息处理成功，返回一个 200 OK 响应。 如果 Dapr 收到超过 200 的返回状态代码，或者你的应用崩溃，Dapr 将根据 At-Least-Once 语义尝试重新传递消息
+    /// </summary>
+    /*
+        要使用这个主题范围，可以设置一个 pub/sub 组件的三个元数据属性：
+        spec.metadata.publishingScopes
+        分号分隔应用程序列表& 逗号分隔的主题列表允许该 app 发布信息到主题列表
+        如果在 publishingScopes (缺省行为) 中未指定任何内容，那么所有应用程序可以发布到所有主题
+        要拒绝应用程序发布信息到任何主题，请将主题列表留空 (app1=;app2=topic2)
+        例如， app1=topic1;app2=topic2,topic3;app3= 允许 app1 发布信息至 topic1 ，app2 允许发布信息到 topic2 和 topic3 ，app3 不允许发布信息到任何主题。
+        spec.metadata.subscriptionScopes
+        分号分隔应用程序列表& 逗号分隔的主题列表允许该 app 订阅主题列表
+        如果在 subscriptionScopes (缺省行为) 中未指定任何内容，那么所有应用程序都可以订阅所有主题
+        例如， app1=topic1;app2=topic2,topic3 允许 app1 订阅 topic1 ，app2 可以订阅 topic2 和 topic3
+        spec.metadata.allowedTopics
+        一个逗号分隔的允许主题列表，对所有应用程序。
+        如果未设置 allowedTopics (缺省行为) ，那么所有主题都有效。 subscriptionScopes 和 publishingScopes 如果存在则仍然生效。
+        publishingScopes 或 subscriptionScopes 可用于与 allowedTopics 的 conjuction ，以添加限制粒度
+     */
     public class PublishSubscribeController : ControllerBase
     {
         private readonly ILogger<PublishSubscribeController> _logger;
@@ -65,7 +87,12 @@ namespace daprA.Controllers.PublishSubscribe
         [HttpGet("ProgrammaticPub")]
         public async Task<ActionResult> ProgrammaticPubAsync()
         {
-            await _daprClient.PublishEventAsync(RBMQ_PUB_SUN,Programmatic_TOPIC_NAME, $"RBMQ_Programmatic:{Guid.NewGuid()}");
+            //Dapr 允许对每个消息设置生存时间(TTL)。 这意味着应用程序可以设置每条消息的生存时间，并且这些消息过期后订阅者不会收到。
+            var dictionary = new Dictionary<string, string>()
+            {
+                { "ttlInseconds","120"}
+            };
+            await _daprClient.PublishEventAsync(RBMQ_PUB_SUN, Programmatic_TOPIC_NAME, $"RBMQ_Programmatic:{Guid.NewGuid()}", metadata: dictionary);
             await _daprClient.PublishEventAsync(Redis_PUB_SUN, Programmatic_TOPIC_NAME, $"Redis_Programmatic:{Guid.NewGuid()}");
             return Ok();
         }
